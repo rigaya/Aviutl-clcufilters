5ec61590dca152aec9348a3b02039c50517b6939
rigaya <rigaya34589@live.jp>
2022-09-17 07:16:18 +0900

    QSVEncの更新を反映。

--- filter_core/rgy_prm.h
+++ filter_core/rgy_prm.h
@@ -49,7 +49,7 @@ static const int DEFAULT_IGNORE_DECODE_ERROR = 10;
 #endif
 #define ENABLE_VPP_FILTER_AFS          (ENCODER_QSV   || ENCODER_NVENC || ENCODER_VCEENC)
 #define ENABLE_VPP_FILTER_NNEDI        (ENCODER_QSV   || ENCODER_NVENC || ENCODER_VCEENC || CLFILTERS_AUF)
-#define ENABLE_VPP_FILTER_YADIF        (ENCODER_NVENC)
+#define ENABLE_VPP_FILTER_YADIF        (ENCODER_QSV   || ENCODER_NVENC || ENCODER_VCEENC)
 #define ENABLE_VPP_FILTER_RFF          (ENCODER_NVENC)
 #define ENABLE_VPP_FILTER_SELECT_EVERY (ENCODER_NVENC)
 #define ENABLE_VPP_FILTER_DECIMATE     (ENCODER_QSV   || ENCODER_NVENC || ENCODER_VCEENC)
@@ -180,6 +180,16 @@ static const int   FILTER_DEFAULT_DEBAND_SEED = 1234;
 static const bool  FILTER_DEFAULT_DEBAND_BLUR_FIRST = false;
 static const bool  FILTER_DEFAULT_DEBAND_RAND_EACH_FRAME = false;
 
+enum class RGYHEVCBsf {
+    INTERNAL,
+    LIBAVCODEC
+};
+
+const CX_DESC list_hevc_bsf_mode[] = {
+    { _T("internal"),   (int)RGYHEVCBsf::INTERNAL   },
+    { _T("libavcodec"), (int)RGYHEVCBsf::LIBAVCODEC },
+    { NULL, 0 }
+};
 
 const CX_DESC list_vpp_denoise[] = {
     { _T("none"),    0 },
@@ -1281,6 +1291,7 @@ struct RGYParamCommon {
     RGYAVSync AVSyncMode;     //avsyncの方法 (NV_AVSYNC_xxx)
     bool timecode;
     tstring timecodeFile;
+    RGYHEVCBsf hevcbsf;
 
     RGYVideoQualityMetric metric;
 
