596ca90326f99e3f59aa82088e8451e40c2eaf37
rigaya <rigaya34589@live.jp>
2025-01-05 21:31:05 +0900

    エンコーダの更新を反映。

--- filter_core/rgy_cmd.cpp
+++ filter_core/rgy_cmd.cpp
@@ -4880,7 +4880,17 @@ int parse_one_common_option(const TCHAR *option_name, const TCHAR *strInput[], i
     }
     if (IS_OPTION("trim")) {
         i++;
-        auto trim_str_list = split(strInput[i], _T(","));
+        tstring trim_arg_str = strInput[i];
+        // strInput[i]が "Trim" で始まるかで判定し、AviSynthのTrim形式を変換する
+        if (0 == _tcsncmp(tolowercase(trim_arg_str).c_str(), _T("trim"), _tcslen(_T("trim")))) {
+            trim_arg_str = tolowercase(trim_arg_str);
+            trim_arg_str = str_replace(trim_arg_str, _T(" "), _T("")); // 空白除去
+            trim_arg_str = str_replace(trim_arg_str, _T(","), _T(":")); // "," -> ":"
+            trim_arg_str = str_replace(trim_arg_str, _T(")++trim("), _T(",")); // ")++trim(" -> ","
+            trim_arg_str = str_replace(trim_arg_str, _T("trim("), _T(""));
+            trim_arg_str = str_replace(trim_arg_str, _T(")"), _T(""));
+        }
+        auto trim_str_list = split(trim_arg_str, _T(","));
         std::vector<sTrim> trim_list;
         for (auto trim_str : trim_str_list) {
             sTrim trim;
@@ -9043,10 +9053,10 @@ tstring gen_cmd_help_vpp() {
         if (ENABLE_NVVFX) {
             str += strsprintf(_T("\n")
                 _T("      superres-mode=<int>\n")
-                _T("        mode for nvvfx-superres     0 ... conservative (default)\n")
-                _T("                                    1 ... aggressive \n")
+                _T("        mode for nvvfx-superres     0 ... conservative\n")
+                _T("                                    1 ... aggressive (default)\n")
                 _T("      superres-strength=<float>\n")
-                _T("        strength for nvvfx-superres (0.0 - 1.0)\n"));
+                _T("        strength for nvvfx-superres (0.0 - 1.0, default = 0.4)\n"));
         }
         if (ENABLE_NVSDKNGX) {
             str += strsprintf(_T("\n")
